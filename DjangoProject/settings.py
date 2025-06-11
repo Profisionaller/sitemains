ac517be619e58a02bf2a247bdffcd97b0f051740
Sasha <sashatryes@gmail.com>
2025-06-12 00:30:24 +0300

    Laba 5(2)

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -55,11 +55,11 @@ ROOT_URLCONF = 'DjangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',  # 🔹 Додай цю стрічку
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
