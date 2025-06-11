9d024cb6daa54ddef88b539bb74c15f9c4b97d87
Sasha <sashatryes@gmail.com>
2025-06-11 23:42:38 +0300

    Laba 5(1)

--- myapp/models.py
+++ myapp/models.py
@@ -22,7 +22,10 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     rating = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=10)
     image_url = models.URLField(blank=True, null=True)
+
+
 
     def __str__(self):
         return f"{self.name} - ${self.price}"
